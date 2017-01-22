53f9f054684263aefe55f1502bcf29a4ecdf5e45
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-01-22 11:37:15 +0100

    Add command for listing known templates

--- lib/arc42-pandoc/markdown/markdown_builder.rb
+++ lib/arc42-pandoc/markdown/markdown_builder.rb
@@ -11,16 +11,26 @@ module Arc42Pandoc
     def create(output, template = nil, options = {})
       options = sanatize_options(options)
 
-      template = File.join(File.dirname(__FILE__), 'templates', options[:lang].to_s, 'basic.md') if template.nil?
+      template = File.join(dirname, 'templates', options[:lang].to_s, 'basic.md') if template.nil?
 
       ruyml = Ruyml::Data.new(options)
       ruyml.render_file(template, output)
     end
 
     def list
+      files = Dir[File.join(dirname, 'templates', '**/*')].select { |file| File.file?(file) }
+      chunks = files.group_by { |file| file.split('/')[-2] }
+
+      chunks.each { |lang, templates|
+        puts "Templates available for language '#{lang}': \n  * #{templates.map { |t| t.split('/')[-1] }.join("\n\t* ")}\n\n"
+      }
     end
 
     private
+
+    def dirname
+      File.dirname(__FILE__)
+    end
 
     def sanatize_options(options)
       options = Map[options]
