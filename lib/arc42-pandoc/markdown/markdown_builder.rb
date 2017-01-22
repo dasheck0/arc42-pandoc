1b8e5f43a34c8e771fbf3bebc73c3293fe74108c
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-01-22 12:20:57 +0100

    Respect template flag when generating markdown file

--- lib/arc42-pandoc/markdown/markdown_builder.rb
+++ lib/arc42-pandoc/markdown/markdown_builder.rb
@@ -8,10 +8,10 @@ module Arc42Pandoc
 
     end
 
-    def create(output, template = nil, options = {})
+    def create(output, options = {})
       options = sanatize_options(options)
 
-      template = File.join(dirname, 'templates', options[:language].to_s, 'basic.md') if template.nil?
+      template = find_template(options[:template], options[:language])
       abort "The template is not available for the language '#{options[:language]}'. Check 'arc42 list' for available templates." unless File.file?(template)
 
       ruyml = Ruyml::Data.new(options)
@@ -29,6 +29,11 @@ module Arc42Pandoc
 
     private
 
+    def find_template(template, language)
+      template = template.split('.')[0]
+      File.join(dirname, 'templates', language.to_s, "#{template}.md")
+    end
+
     def dirname
       File.dirname(__FILE__)
     end
@@ -38,6 +43,7 @@ module Arc42Pandoc
       options[:language] = :en if options[:language].nil?
       options[:title] = '' if options[:title].nil?
       options[:author] = '' if options[:author].nil?
+      options[:template] = 'basic' if options[:template].nil?
 
       options
     end
