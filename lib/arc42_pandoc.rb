1dd25d884a7a3bb5103813bde26b2cc3db9f2d7a
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-02-03 09:17:19 +0100

    Fix bug in require

--- lib/arc42_pandoc.rb
+++ lib/arc42_pandoc.rb
@@ -1,6 +1,6 @@
 require_relative 'arc42-pandoc/version'
 require_relative 'arc42-pandoc/markdown/markdown_builder'
-require_relative 'arc42-pandoc/markdown/apiary_builder'
+require_relative 'arc42-pandoc/apiary/apiary_builder'
 
 module Arc42Pandoc
 
